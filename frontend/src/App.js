6acbcdf0880bfc74c75632566936cb50d068ad05
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2022-10-14 22:56:19 -0400

    compare page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Layout from './pages/layout.jsx';
 import { Route, Routes } from 'react-router-dom';
+import Compare from './pages/compare';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          
+          <Route path="/compare" element={<Compare />} />
         </Route>
       </Routes>
     </BrowserRouter>
